4cb1e8ba1c0b955ae5954f54adbf1246f321526f
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-05-11 19:51:36 -0300

    adiciona modelo Usuario

---
+++ src/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducaLivros.Models;
+
+[Table("usuarios")]
+public class Usuario
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar o nome.")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar o e-mail")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar a senha")]
+    public string Senha { get; set; }
+}
