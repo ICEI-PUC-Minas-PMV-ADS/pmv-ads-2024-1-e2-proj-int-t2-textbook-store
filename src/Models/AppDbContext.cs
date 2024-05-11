dd5da990ec9c3e9e1cb212ad1b3646099b67a4c2
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-05-11 20:33:22 -0300

    adiciona model e migration de  Anuncio

--- src/Models/AppDbContext.cs
+++ src/Models/AppDbContext.cs
@@ -8,4 +8,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Usuario> Usuarios { get; set; }
+
+    public DbSet<Anuncio> Anuncios { get; set; }
 }
